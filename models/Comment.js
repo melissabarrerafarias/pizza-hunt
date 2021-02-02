13f27280a43503813c36ea88e3961eb1c7f26b7c
unknown <melissikiss@gmail.com>
2021-02-02 12:27:45 -0800

    created ReplySchema and got sum of replies + comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,58 @@
-const { Schema, model } = require('mongoose'); 
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({ 
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+const CommentSchema = new Schema({
     writtenBy: {
         type: String
-    }, 
+    },
     commentBody: {
         type: String
-    }, 
+    },
     createdAt: {
-        type: Date, 
-        default: Date.now
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema] // associates comment with replies
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-}); 
+);
 
-const Comment = model('Comment', CommentSchema); 
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
