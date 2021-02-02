ddeec719347b726662a95db3d6f6626607c0de98
unknown <melissikiss@gmail.com>
2021-02-02 10:49:34 -0800

    created comment model and functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,40 @@
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({ // schema constructor from mongoose 
-    pizzaName: {
-      type: String // adheres to the built in JavaScript data types
+  pizzaName: {
+    type: String // adheres to the built in JavaScript data types
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],  // indicates array as data type
+  comments: [
+    { // expects ObjectId and its data to come from Comment model
+      type: Schema.Types.ObjectId,
+      ref: 'Comment' // tells Pizza model which document to search 
+    }
+  ],
+},
+  {
+    toJSON: {
+      virtuals: true,
     },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: [] // indicates array as data type 
-  });
+    id: false
+  }
+);
+
+//get total count of comments and replies on retrieval 
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
