e49ef2c3e739b3da867a948b124a7ad2d3d5fc4a
unknown <melissikiss@gmail.com>
2021-02-02 12:50:19 -0800

    added routes for addReply and removeReply

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
-// /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
 
-// /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId').post(addComment); // adds comment 
+
+router
+.route('/:pizzaId/:commentId') // redirects to same endpoint for both methods -> 
+.put(addReply) // adds comment reply
+.delete(removeComment) // removes comment 
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply); // removes reply 
+
+
+
+
 
 module.exports = router;
