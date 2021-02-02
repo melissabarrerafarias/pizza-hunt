9a82c563b775fc65062d3c0841c348f825d657fd
unknown <melissikiss@gmail.com>
2021-02-02 11:32:58 -0800

    added getters to Pizza model for date formatting

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat'); 
 
 const PizzaSchema = new Schema({ // schema constructor from mongoose 
   pizzaName: {
@@ -9,7 +10,8 @@ const PizzaSchema = new Schema({ // schema constructor from mongoose
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now, 
+    get: (createdAtVal) => dateFormat(createdAtVal)
   },
   size: {
     type: String,
@@ -26,6 +28,7 @@ const PizzaSchema = new Schema({ // schema constructor from mongoose
   {
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false
   }
